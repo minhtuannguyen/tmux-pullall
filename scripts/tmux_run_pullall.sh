51db7bf4a61d1a9093d10783c1760f710948e650
Minh Tuan Nguyen <minhtuannguyen@MrTsAir.fritz.box>
2021-08-17 16:05:01 +0200

    correct the dir path to be pulled all

--- scripts/tmux_run_pullall.sh
+++ scripts/tmux_run_pullall.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-find . -maxdepth 3 -name .git -type d | rev | cut -c 6- | rev | xargs -I {} sh -c  "echo \"\n\" {} \"\n\"; git -C {} pull"
+PANE_CURRENT_PATH="$(tmux display-message -p -F "#{pane_current_path}" -t0)"
+
+find $PANE_CURRENT_PATH -maxdepth 3 -name .git -type d | rev | cut -c 6- | rev | xargs -I {} sh -c  "echo \"\n\" {} \"\n\"; git -C {} pull"
+
+echo "Done"
